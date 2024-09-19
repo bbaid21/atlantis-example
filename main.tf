7f58a7991fe4f76aac2fc2a914bd547cdbf50e14
Bhavika Baid <bbaid@twilio.com>
2024-09-19 15:54:21 +0530

    Updating the instance size

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "aws_security_group" "allow_ssh" {
 # Create an EC2 instance
 resource "aws_instance" "web" {
   ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (HVM), SSD Volume Type
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name      = "your-key-pair-name"  # Replace with your key pair name
 
   subnet_id                   = aws_subnet.public.id
